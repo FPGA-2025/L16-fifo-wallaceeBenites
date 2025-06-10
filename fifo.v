c6785fc6a0795cb95bd43cf078f94b0e1895c7cc
wallaceeBenites <wallacepuck@gmail.com>
2025-06-09 22:39:38 -0300

    teste

--- fifo.v
+++ fifo.v
@@ -9,7 +9,40 @@ module fifo(
     output empty
 );
 
-// insira seu código aqui
+reg [7:0] fila_fifo [0:3]; 
+
+reg [1:0] wr_pointer; 
+reg [1:0] rd_pointer; 
+
+
+assign full  = (((wr_pointer + 1) == rd_pointer) || (wr_pointer == 3 && rd_pointer == 0));
+assign empty = (wr_pointer == rd_pointer);
+
+always @(posedge clk, negedge rstn) begin
+    if(~rstn)begin
+        wr_pointer <= 0;
+    end
+    else begin
+        if (~full && wr_en) begin
+            fila_fifo[wr_pointer] <= data_in;
+
+            wr_pointer <= wr_pointer + 1;
+        end
+    end
+end
+
+always @(posedge clk, negedge rstn) begin
+    if(~rstn)begin
+        rd_pointer <= 0;
+    end
+    else begin
+        if (~empty && rd_en) begin
+            data_out <= fila_fifo[rd_pointer];
+
+            rd_pointer <= rd_pointer + 1;
+        end
+    end
+end
 
 endmodule
 
